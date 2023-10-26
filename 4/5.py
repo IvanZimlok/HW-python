dbabb6cb42957cf112cf8178a72e89058ce76fda
IvanZimlok <ivanporam@gmail.com>
2023-10-26 11:58:42 +0300

    test2

--- 4/5.py
+++
@@ -1,6 +0,0 @@
-cost = int(input())
-while cost != 0:
-    skidka = int(input())
-    cost -= skidka
-    if cost < 0:
-        cost = 0
--- 4/6.py
+++
@@ -1,9 +0,0 @@
-cost = int(input())
-if cost %2 == 0:
-    while cost != 0:
-        if cost % 2 == 0:
-            cost // 2
-        else:
-            print('К оплате: ' + str(cost))
-else:
-    print('К оплате: ' + str(cost*0.85))
